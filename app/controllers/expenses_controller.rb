63a04113b15a14b658bb37e9971e62afcfe58e6b
guterres <guterres25@gmail.com>
2015-05-15 00:57:11 +0100

    controllers expense

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,58 @@
+class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @expenses = Expense.all
+  end
+
+  def show
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def edit
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    if @expense.create
+      flash[:notice] = 'Expense was successfully created'
+      redirect_to @expense
+    else
+      redirect_to :new
+      flash[:error] = 'Please try again!'
+    end
+  end
+
+  def update
+    if @expense.update(expense_params)
+      render :show
+      flash[:notice] = 'Expense was successfully updated'
+    else
+      render :edit
+      flash[:error] = 'Please try again!'
+    end
+  end
+
+  def destroy
+    if @expense.destroy
+      flash[:notice] = 'Expense was successfully destroyed'
+      redirect_to expenses_url
+    else
+      redirect_to :back
+      flash[:error] = 'Please try again!'
+    end
+  end
+
+  private
+
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:date, :user_id, :category_id, :description, :amount, :document)
+  end
+end
