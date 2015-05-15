5310f708db7d8a6b4e7aaf6668bed4fa11f0fca1
guterres <guterres25@gmail.com>
2015-05-15 22:06:44 +0100

    controller revenue

---
+++ app/controllers/revenues_controller.rb
@@ -0,0 +1,61 @@
+class RevenuesController < ApplicationController
+  before_action :set_revenue, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @revenues = Revenue.all
+  end
+
+  def show
+  end
+
+  def new
+    @revenue = Revenue.new
+    @categories = Category.all
+  end
+
+  def edit
+    @categories = Category.all
+  end
+
+  def create
+    @revenue = Revenue.new(revenue_params)
+    @revenue.user_id = current_user.id
+    if @revenue.save
+      flash[:notice] = 'Revenue was successfully created'
+      redirect_to @revenue
+    else
+      redirect_to :new
+      flash[:error] = 'Please try again!'
+    end
+  end
+
+  def update
+    if @revenue.update(revenue_params)
+      render :show
+      flash[:notice] = 'Revenue was successfully updated'
+    else
+      render :edit
+      flash[:error] = 'Please try again!'
+    end
+  end
+
+  def destroy
+    if @revenue.destroy
+      flash[:notice] = 'Revenue was successfully destroyed'
+      redirect_to revenues_url
+    else
+      redirect_to :back
+      flash[:error] = 'Please try again!'
+    end
+  end
+
+  private
+
+  def set_revenue
+    @revenue = Revenue.find(params[:id])
+  end
+
+  def revenue_params
+    params.require(:revenue).permit(:date, :user_id, :category_id, :description, :amount, :document)
+  end
+end
