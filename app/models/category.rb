cdc3d228f320efcd7a66460f297122134977dbd7
guterres <guterres25@gmail.com>
2015-05-15 21:56:46 +0100

    model revenue

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   belongs_to :expense
+  belongs_to :revenue
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515202152) do
+ActiveRecord::Schema.define(version: 20150515205321) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 20150515202152) do
     t.integer  "document_file_size"
     t.datetime "document_updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "revenues", force: :cascade do |t|
+    t.integer  "user_id"
+    t.date     "date"
+    t.integer  "category_id"
+    t.string   "description"
+    t.float    "amount"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "document_file_name"
+    t.string   "document_content_type"
+    t.integer  "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
