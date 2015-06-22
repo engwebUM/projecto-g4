123ae5d9d086c76a696a93dbd25ff66fb7fcb52b
DuarteDuarte <dd3m@hotmail.com>
2015-06-22 09:11:25 +0100

    fixed same user expenses and revenues - trying to implement combination chart for categories

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,29 +1,60 @@
 module StatsHelper
   def create_charts(expenses, revenues)
-    chart_data1 = expenses.pluck('user_id', 'amount')
+    chart_data1 = expenses.group(:user_id).sum(:amount).to_a
     chart_data1.each do |ex|
       ex[0] = User.find(ex[0]).email
     end
-    chart_data2 = revenues.pluck('user_id', 'amount')
+    
+    chart_data2 = revenues.group(:user_id).sum(:amount).to_a
     chart_data2.each do |ex|
       ex[0] = User.find(ex[0]).email
     end
-    chart_data3 = expenses.pluck('user_id', 'amount')
-    chart_data3.each do |ex|
-      ex[0] = User.find(ex[0]).email
+
+
+
+    aux4 = Expense.all.group(:category_id).sum(:amount)
+    aux5 = Revenue.all.group(:category_id).sum(:amount)
+    
+    keys = [aux4,aux5].flat_map(&:keys).uniq
+    
+    keys.map do |k| 
+      {k => [{value1: aux4[k] || "0"},
+            {value2: aux5[k] || "0"}]} 
     end
-    chart_data4 = expenses.pluck('user_id', 'amount')
-    chart_data4.each do |ex|
-      ex[0] = User.find(ex[0]).email
+    
+    chaves = keys.reduce(&:merge)
+
+    chaves = []
+    keys.each do |k,value|
+      chaves << k
     end
 
-    @chart1 = draw(chart_data1,'Expenses by User')
-    @chart2 = draw(chart_data2,'Revenues by User')
-    @chart3 = draw(chart_data1,'Expenses by User')
-    @chart4 = draw(chart_data2,'Revenues by User')
+    @chart1 = draw_pie(chart_data1,'Expenses by User')
+    @chart2 = draw_pie(chart_data2,'Revenues by User')
+    @chart3 = draw_pie(chart_data1,'Expenses by User')
+    @chart4 = draw_combination(keys,chaves,'Revenues by User')
   end
 
-  def draw (chart_data,title)
+
+#keys tem [{1=>[{:value1=>50},{:value2=>87}]},{2=>[{:value1=>50},{:value2=>99}]}]
+
+  def draw_combination(hash_data,chaves,title)
+    chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title({ :text=>"Combination chart"})
+      f.options[:xAxis][:categories] = chaves
+      f.labels(:items=>[:html=>"Total fruit consumption", :style=>{:left=>"40px", :top=>"8px", :color=>"black"} ])      
+      hash_data.each do |fds|
+        y = fds.to_a
+        key = y[0][0]
+        value = y[0][1]
+        a = value[0][:value1]
+        b = value[1][:value2]
+        f.series(:type=> 'column',:name=> key,:data=> [a,b])
+      end
+    end
+  end
+
+  def draw_pie(chart_data,title)
     chart = LazyHighCharts::HighChart.new('pie') do |f|
       f.chart defaultSeriesType: 'pie', margin: [50, 200, 60, 170]
       series = {
