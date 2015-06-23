2c369783e1343f88d78cafec9ee5df0b45fff3f4
DuarteDuarte <dd3m@hotmail.com>
2015-06-23 20:12:48 +0100

    Fixed rubocop

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -9,21 +9,23 @@ module StatsHelper
     @chart3 = draw_combination(category_exp_rev)
     @chart4 = draw_two_bar(category_exp_rev)
   end
-  
+
   def draw_total
     total_expenses = Expense.all.sum(:amount)
     total_revenues = Revenue.all.sum(:amount)
-    chart = LazyHighCharts::HighChart.new('column') do |f|
+    total_bar_chart(total_expenses, total_revenues)
+  end
+
+  def total_bar_chart(total_expenses, total_revenues)
+    LazyHighCharts::HighChart.new('column') do |f|
       f.chart defaultSeriesType: 'column', margin: [50, 200, 60, 170]
-      series = { type: 'column', name: 'Total Expenses and Revenues', data: [total_expenses,total_revenues] }
+      series = { type: 'column', name: 'Total Expenses and Revenues', data: [total_expenses, total_revenues] }
       f.series(series)
       f.options[:title][:text] = 'Total Expenses and Revenues'
       f.legend(layout: 'vertical', style: { left: 'auto', bottom: 'auto', right: '50px', top: '100px' })
       f.plot_options(pie: { allowPointSelect: true, cursor: 'pointer', dataLabels: { enabled: true, color: 'black', style: { font: '13px Trebuchet MS, Verdana, sans-serif' } } })
     end
-    chart
   end
-
 
   def expenses_by_user
     chart_data1 = Expense.all.group(:user_id).sum(:amount).to_a
