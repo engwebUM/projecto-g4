a6c00610e8dc299a0bc37524f52be494e06baefe
DuarteDuarte <dd3m@hotmail.com>
2015-06-23 05:06:20 +0100

    Fixed rubocop issues, and code refactored
    had to ignore method length and AbcSize in one method due to the need of
    creating and updating 4 variables

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -6,8 +6,8 @@ module StatsHelper
 
     @chart1 = draw_pie(chart_data1, 'Expenses by User')
     @chart2 = draw_pie(chart_data2, 'Revenues by User')
-    @chart3 = draw_bars(category_exp_rev)
-    @chart4 = draw_combination(category_exp_rev)
+    @chart3 = draw_combination(category_exp_rev)
+    @chart4 = draw_two_bar(category_exp_rev)
   end
 
   def expenses_by_user
@@ -38,53 +38,61 @@ module StatsHelper
     novo
   end
 
-  def draw_bars(hash_data)
-    chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: 'Expenses and Revenues by Category')
-      f.options[:xAxis][:categories] = hash_data.reduce(&:merge).keys
-      f.labels(items: [html: 'Expenses and Revenues by Category', style: { left: '20px', top: '0px', color: 'black' }])
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def draw_combination(hash_data)
+    LazyHighCharts::HighChart.new('graph') do |f|
+      combination_options(f, hash_data)
       expenses_list = []
       revenues_list = []
       id_exp_amount = []
       id_rev_amount = []
       hash_data.each do |hash|
-        values = hash.to_a
-        expenses_list << values[0][1][0][:value1]
-        revenues_list << values[0][1][1][:value2]
-        a = values[0][1][0][:value1]
-        b = values[0][1][1][:value2]
-        key = values[0][0]
-        id_exp_amount << { name: key, y: a }
-        id_rev_amount << { name: key, y: b }
+        element = hash.to_a.first
+        key = element[0]
+        value1 = element[1][0]
+        value2 = element[1][1]
+        expenses_list << value1[:value1]
+        revenues_list << value2[:value2]
+        id_exp_amount << { name: key, y: value1[:value1] }
+        id_rev_amount << { name: key, y: value2[:value2] }
       end
-      f.series(type: 'column', name: 'Expenses', data: expenses_list)
-      f.series(type: 'column', name: 'Revenues', data: revenues_list)
-      f.series(
-        type: 'pie', name: 'Total Expenses', data: id_exp_amount, center: [25, 50], size: 100, showInLegend: false,
-        dataLabels: {
-          enabled: false
-        }
-      )
-      f.series(
-        type: 'pie', name: 'Total Revenues', data: id_rev_amount, center: [175, 50], size: 100, showInLegend: false,
-        dataLabels: {
-          enabled: false
-        }
-      )
+      combination_series(f, expenses_list, revenues_list, id_exp_amount, id_rev_amount)
     end
-    chart
+  end
+  # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+  def combination_options(f, hash_data)
+    f.title(text: 'Expenses and Revenues by Category')
+    f.options[:xAxis][:categories] = hash_data.reduce(&:merge).keys
+    f.labels(items: [html: 'Expenses and Revenues by Category', style: { left: '20px', top: '0px', color: 'black' }])
   end
 
-  def draw_combination(hash_data)
+  def combination_series(f, expenses_list, revenues_list, id_exp_amount, id_rev_amount)
+    f.series(type: 'column', name: 'Expenses', data: expenses_list)
+    f.series(type: 'column', name: 'Revenues', data: revenues_list)
+    f.series(type: 'pie', name: 'Total Expenses', data: id_exp_amount, center: [25, 50], size: 100, showInLegend: false, dataLabels: { enabled: false })
+    f.series(type: 'pie', name: 'Total Revenues', data: id_rev_amount, center: [175, 50], size: 100, showInLegend: false, dataLabels: { enabled: false })
+  end
+
+  def draw_two_bar(hash_data)
     LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: 'Expenses and Revenues by Category')
-      f.options[:xAxis][:categories] = %w(Exp Rev)
+      two_bar_options f
       hash_data.each do |hash|
-        values = hash.to_a
-        value = values[0]
-        f.series(type: 'column', name: value[0], data: [value[1][0][:value1], value[1][1][:value2]])
+        hash_element = hash.to_a
+        element = hash_element[0]
+        value_value = element[1]
+        two_bar_series(f, element[0], [value_value[0][:value1], value_value[1][:value2]])
       end
     end
+  end
+
+  def two_bar_options(f)
+    f.title(text: 'Expenses and Revenues by Category')
+    f.options[:xAxis][:categories] = %w(Exp Rev)
+  end
+
+  def two_bar_series(f, name, data)
+    f.series(type: 'column', name: name, data: data)
   end
 
   def draw_pie(chart_data, title)
