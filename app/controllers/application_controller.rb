99a53ebee42ee67f1a6bed6d0c2d05094c90c233
guterres <guterres25@gmail.com>
2015-06-25 22:36:59 +0100

    fix path users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  private
+
+  def after_invite_path_for(resource)
+    users_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+
   belongs_to :expense
   belongs_to :revenue
 end
